923ce735475e532661e5ec4a74e0c64a722b59b4
at-thachnguyen <thach.nguyen@asiantech.vn>
2018-03-20 17:02:52 +0700

    temp

---
+++ config/deploy/staging.rb
@@ -0,0 +1,16 @@
+server "35.185.188.216",
+  user: "root",
+  roles: %w{web db app},
+  ssh_options: {
+    keys: %w(~/.ssh/gg_cloud_duydy),
+    forward_agent: false,
+    auth_methods: %w(publickey password)
+    # password: "please use keys"
+  }
+
+  # namespace :deploy do
+  #   desc 'Restart Unicorn'
+  #   task :restart_unicorn do
+  #     invoke 'unicorn:legacy_restart'
+  #   end
+  # end
